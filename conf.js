d3452628deabcc456b2b7edfa4fc1a5fcf2ce0b8
System Administrator <root@rakeshs-MacBook-Air.local>
2019-01-21 20:57:57 +0530

    Added report

--- conf.js
+++ conf.js
@@ -1,3 +1,4 @@
+var HtmlReporter = require('protractor-beautiful-reporter');
 exports.config = {
     // launch locally when fields directConnect and seleniumAddress are not provided
     chromeDriver: '/Users/rakeshrane/Tools/chromedriver',
@@ -5,6 +6,18 @@ exports.config = {
     specs: ['/Users/rakeshrane/ProtractorDemo/JSFilesLocation/POM/Spec/*-spec.js'],
     capabilities: {
       browserName: 'chrome'
-    }
-  }
+    },
+ 
+   onPrepare: function() {
+      // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+      jasmine.getEnv().addReporter(new HtmlReporter({
+         baseDirectory: 'tmp/screenshots',
+         excludeSkippedSpecs: true,
+         takeScreenShotsForSkippedSpecs: true,
+         takeScreenShotsOnlyForFailedSpecs: true,
+         docTitle: 'Protractor Test report',
+         docName: 'Protractor Test report.html',
+      }).getJasmine2Reporter());
+   }
+}
   
